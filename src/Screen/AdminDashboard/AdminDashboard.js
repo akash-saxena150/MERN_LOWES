7020160017aa13f06f9f696569499e2439fbe973
aks-gsg <akash@getsetgo.fitness>
2020-11-03 17:18:28 +0530

    Modified admin dashboard

--- src/Screen/AdminDashboard/AdminDashboard.js
+++ src/Screen/AdminDashboard/AdminDashboard.js
@@ -1,16 +1,71 @@
 import React, { Component } from "react";
 import DummyData from "../../config/dummyData.json";
-import { Grid, AppBar, Toolbar, IconButton } from "@material-ui/core";
+import {
+  Grid,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography
+} from "@material-ui/core";
+import { colors } from "../../service.js";
 class AdminDashboard extends Component {
   render() {
     return (
-      <Grid container>
+      <>
         <AppBar position='static'>
           <Toolbar>
-            <IconButton>A</IconButton>
+            <IconButton color='inherit'>A</IconButton>
           </Toolbar>
         </AppBar>
-      </Grid>
+        <Grid
+          container
+          justify='center'
+          alignItems='center'
+          spacing={2}
+          style={{ width: "50%", height: "80vh", margin: "0 auto" }}
+        >
+          <Grid container justify='center' alignItems='center' item xs={6}>
+            <Grid
+              container
+              justify='center'
+              alignItems='center'
+              className='block'
+            >
+              <Typography variant='subtitle1'>Users</Typography>
+            </Grid>
+          </Grid>
+          <Grid justify='center' alignItems='center' item xs={6}>
+            <Grid
+              container
+              justify='center'
+              alignItems='center'
+              className='block'
+            >
+              <Typography variant='subtitle1'>Domains</Typography>
+            </Grid>
+          </Grid>
+          <Grid justify='center' alignItems='center' item xs={6}>
+            <Grid
+              container
+              justify='center'
+              alignItems='center'
+              className='block'
+            >
+              <Typography variant='subtitle1'>Modules</Typography>
+            </Grid>
+          </Grid>
+          <Grid justify='center' alignItems='center' item xs={6}>
+            <Grid
+              container
+              justify='center'
+              alignItems='center'
+              className='block'
+            >
+              <Typography variant='subtitle1'>Graphs</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </>
     );
   }
 }
