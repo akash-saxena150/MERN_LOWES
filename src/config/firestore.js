77ccbf4da51826a040b055e0addcb8275f881362
aks-gsg <akash@getsetgo.fitness>
2020-11-03 17:01:17 +0530

    Added dummy data

---
+++ src/config/firestore.js
@@ -0,0 +1,24 @@
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+// <script src="https://www.gstatic.com/firebasejs/8.0.0/firebase-analytics.js"></script>
+
+// <script>
+//   // Your web app's Firebase configuration
+//   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyCKNGFI2spbTynTRpeLH5-gtnvS-yy0Go0",
+//     authDomain: "mern-lowes.firebaseapp.com",
+//     databaseURL: "https://mern-lowes.firebaseio.com",
+//     projectId: "mern-lowes",
+//     storageBucket: "mern-lowes.appspot.com",
+//     messagingSenderId: "962437401092",
+//     appId: "1:962437401092:web:2818f6a5709fa5c8d2c351",
+//     measurementId: "G-1R74WBG52J"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
+// </script>
